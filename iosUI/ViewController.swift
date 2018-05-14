0b259df2f551122668a83fa5e719113a7ca82f78
liquidjoo <liquidjoo@gmail.com>
2018-05-15 01:10:44 +0900

    Interface Builder Action 테스트

--- iosUI/ViewController.swift
+++ iosUI/ViewController.swift
@@ -16,9 +16,22 @@ class ViewController: UIViewController {
         let btn = UIButton(type: UIButtonType.system)
         btn.frame = CGRect(x: 50, y: 100, width: 150, height: 30)
         btn.setTitle("테스트 버튼", for: UIControlState.normal)
-        btn.center = CGPoint(x: btn.frame.size.width / 2, y: 100)
+        
+        // 버튼을 수평 중앙 정렬
+        btn.center = CGPoint(x: self.view.frame.size.width / 2, y: 100)
         
         self.view.addSubview(btn)
+        
+        // 버튼의 이벤트와 메소드 btnOnClick(_:)을 연결한다.
+        btn.addTarget(self, action: #selector(btnOnClik(_:)), for: .touchUpInside)
+    }
+    
+    @objc func btnOnClik(_ sender:Any) {
+        // 호출한 객체가 버튼이라면
+        if let btn = sender as? UIButton {
+            btn.setTitle("클릭되었습니다", for: UIControlState.normal)
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
