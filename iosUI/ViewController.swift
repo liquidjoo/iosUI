5823f0e7575e79558ea2f6f25617ba09d5bf65e3
liquidjoo <liquidjoo@gmail.com>
2018-05-15 00:25:10 +0900

    UI Button Test

--- iosUI/ViewController.swift
+++ iosUI/ViewController.swift
@@ -13,6 +13,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let btn = UIButton(type: UIButtonType.system)
+        btn.frame = CGRect(x: 50, y: 100, width: 150, height: 30)
+        btn.setTitle("테스트 버튼", for: UIControlState.normal)
+        btn.center = CGPoint(x: btn.frame.size.width / 2, y: 100)
+        
+        self.view.addSubview(btn)
     }
 
     override func didReceiveMemoryWarning() {
